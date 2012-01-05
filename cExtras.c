5f2e6f24bab98312aadf91c95d6be542f7558a70
Joel Svensson <svensson.bj@gmail.com>
2012-01-05 20:02:35 +0100

    something is a bit off with the texturing

--- cExtras.c
+++ cExtras.c
@@ -17,7 +17,7 @@ void texturedVLine(int x, int y1, int y2, SDL_Surface *surf,
   int sh = surf->h; 
   int sw = surf->w;
   int clipped_y1 = y1 > 0 ? y1 : 0;
-  int clipped_y2 = y2 < (sh-1) ? y2 : sh - 1;
+  int clipped_y2 = y2 < sh ? y2 : (sh - 1);
   int lineHeight = y2 - y1; 
   int clippedHeight = clipped_y2 - clipped_y1;
   int texHeight = yt2 - yt1; 
@@ -29,14 +29,13 @@ void texturedVLine(int x, int y1, int y2, SDL_Surface *surf,
   int tstart = clipped_yt1 * 64 + xt;  
 
   
-  
   // assume 32bit ints.. (fix) 
   unsigned int *sp =(unsigned int*)surf->pixels;
   unsigned int *tp =(unsigned int*)text->pixels;  
 
 
   for (i = 0; i <= clippedHeight; i ++)  {
-    unsigned int p = tp[tstart + ((int)(i*ratio))*64];
+    unsigned int p = tp[tstart + (int)(i*ratio)*64];
     sp[start + (i*sw)] = p; 
   }  
     
