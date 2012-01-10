d90b4fa6333d20d9353e9967b6afad88b2342667
Joel Svensson <svensson.bj@gmail.com>
2012-01-10 23:26:56 +0100

    update

--- Main.hs
+++ Main.hs
@@ -192,27 +192,27 @@ distance :: Point2D -> Point2D -> Float
 distance (x1, y1) (x2, y2) = 
   sqrt $ fromIntegral (xd*xd+yd*yd)
     where 
-      xd = abs(x2 - x1)
-      yd = abs(y2 - y1)
+      xd = x2 - x1
+      yd = y2 - y1
 
 
 intersectX :: Ray -> Line -> Maybe Vector2D 
 intersectX (Ray r1 d1) (Line p1 p2) =  Just (fst p1,snd r1 + floori_ ysect  )	
   where	
-    d     = fst p1 - fst r1
+    d       = fst p1 - fst r1
     divisor = if fst d1 == 0 then 0.0001 else fromIntegral (fst d1)
-    ratio' = fromIntegral (snd d1) / divisor
-    ratio  = if ratio' == 0.0 then  1.0 else ratio'
-    ysect = fromIntegral d * ratio
+    ratio'  = fromIntegral (snd d1) / divisor 
+    ratio   = if ratio' == 0.0 then  0.0001 else ratio'
+    ysect   = fromIntegral d * ratio
 
 intersectY :: Ray -> Line -> Maybe Vector2D 
 intersectY (Ray r1 d1) (Line p1 p2) =  Just (fst r1 + floori_ xsect ,snd p1 )	
   where	
-    d     = snd p1 - snd r1
+    d       = snd p1 - snd r1
     divisor = if snd d1 == 0 then 0.0001 else fromIntegral (snd d1)
-    ratio' = fromIntegral (fst d1) / divisor
-    ratio  = if ratio' == 0.0 then 1.0 else ratio'
-    xsect = fromIntegral d * ratio
+    ratio'  = fromIntegral (fst d1) / divisor 
+    ratio   = if ratio' == 0.0 then 0.0001 else ratio'
+    xsect   = fromIntegral d * ratio
 
 
 
