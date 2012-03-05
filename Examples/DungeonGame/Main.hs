2cf6015c7781fad669f718620ad38666579c851f
Joel Svensson <svenssonjoel@yahoo.se>
2012-03-05 22:28:13 +0100

    -

--- Examples/DungeonGame/Main.hs
+++ Examples/DungeonGame/Main.hs
@@ -245,8 +245,8 @@ eventLoop vc screen floorTextures wallTextures monster targ (dir,pos) ly = do
                
   withLights lights $ \lights' ->              
     do 
-       sl <- renderWalls3Samples vc
-       -- sl <- renderWalls vc
+       --sl <- renderWalls3Samples vc
+       sl <- renderWalls vc
                          testLevelArr 
                          lights' 
                          (pos,direction dir) 
