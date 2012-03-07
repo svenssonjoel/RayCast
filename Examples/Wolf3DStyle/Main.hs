e3e7909408d85b7d6b9d96ea659551735fd4b506
Joel Svensson <svenssonjoel@yahoo.se>
2012-03-07 22:53:51 +0100

    updated slightly

--- Examples/Wolf3DStyle/Main.hs
+++ Examples/Wolf3DStyle/Main.hs
@@ -103,11 +103,11 @@ testFloor = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], --0
              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  --15
             ]
 
-testLevelArr :: MapType
-testLevelArr = unsafePerformIO$ listMap (16,16) testLevel
+--testLevelArr :: MapType
+--testLevelArr = unsafePerformIO$ listMap (16,16) testLevel
 
-testLevelFloorArr :: MapType 
-testLevelFloorArr = unsafePerformIO$ listMap (16,16) testFloor
+--testLevelFloorArr :: MapType 
+--testLevelFloorArr = unsafePerformIO$ listMap (16,16) testFloor
 
 
 ----------------------------------------------------------------------------
@@ -116,305 +116,6 @@ testLevelFloorArr = unsafePerformIO$ listMap (16,16) testFloor
 walkSpeed      = 32 
 
     
-----------------------------------------------------------------------------      
--- Cast for floors 
-{- 
-newFloorCast :: ViewConfig 
-                -> MapType 
-                -> [Light]
-                -> [Int32]
-                -> View 
-                -> [Surface] 
-                -> Surface 
-                -> IO ()
-newFloorCast vc world lights slices view textures surf = 
-  do 
-    
-    tps <- mapM (\x -> do e <- castPtr `fmap` surfaceGetPixels x; return (e,fromIntegral (surfaceGetWidth x))) textures
-
-    mapM_ (lerpRow_ vc world lights slices tps surf)  edges
-               
-  where 
-    edges = [
-             (y+viewportCenterY vc,(newFloorCastPoint vc world view x1 y,
-                 newFloorCastPoint vc world view x2 y))
-            | y <- [0..(viewportCenterY vc-1)]] 
-    
-    x1 = 0;
-    x2 = vcWindowWidth vc - 1 
-    
-lerpRow_ :: ViewConfig -> MapType -> [Light] -> [Int32] -> [(Pixels,Int32)] -> Surface -> (Int32, ((Float,Float),(Float,Float))) -> IO ()     
-lerpRow_ vc world lights slices tps surf (y,(p1,p2)) = 
-         lerpRowC (wallWidth vc) (modMask vc) (vcWindowWidth vc)
-                  16 16 world 
-                  slices 
-                  (map fst tps)
-                  surf
-                  lights                  
-                  (fromIntegral (length lights))
-                  y
-                  (fst p1) (snd p1)
-                  (fst p2) (snd p2)
-             
-newFloorCast2 :: ViewConfig 
-                 -> MapType 
-                 -> [Light]
-                 -> [Int32]
-                 -> View 
-                 -> [Surface] 
-                 -> Surface 
-                 -> IO ()
-newFloorCast2 vc world lights slices view textures surf = 
-  do 
-    
-    tps <- mapM (\x -> do e <- castPtr `fmap` surfaceGetPixels x; return (e,fromIntegral (surfaceGetWidth x))) textures
-    lerpRows_ vc world lights slices tps surf x1s y1s x2s y2s 
-               
-  where 
-    -- TODO: Storable points. 
-    -- TODO: Storable Pairs of points. (lines)  
-    (x1s,y1s) = unzip edgeL
-    (x2s,y2s) = unzip edgeR
-    
-    edgeL = [newFloorCastPoint vc world view x1 y
-            | y <- [0..(viewportCenterY vc-1)]] 
-    edgeR = [newFloorCastPoint vc world view x2 y
-            | y <- [0..(viewportCenterY vc-1)]] 
-    
-    x1 = 0;
-    x2 = vcWindowWidth vc - 1 
-    
--} 
-
-newFloorCast3 :: ViewConfig 
-                 -> MapType 
-                 -> Lights 
-                 -> [Int32]
-                 -> View 
-                 -> [Surface] 
-                 -> Surface 
-                 -> IO ()
-newFloorCast3 vc world lights slices view textures surf = 
-  do 
-    tps <- mapM (\x -> do e <- castPtr `fmap` surfaceGetPixels x; return (e,fromIntegral (surfaceGetWidth x))) textures
-    lerpRows_2 vc world lights slices tps surf rl 
-               
-  where                 
-    rl =  [mkRealLine (newFloorCastPoint vc world view x1 y)                 
-                      (newFloorCastPoint vc world view x2 y) 
-           | y <- [0..(viewportCenterY vc - 1)]]
-                    
-    x1 = 0;
-    x2 = vcWindowWidth vc - 1 
-
-
-{- 
-lerpRows_ :: ViewConfig 
-             -> MapType 
-             -> [Light] 
-             -> [Int32] 
-             -> [(Pixels,Int32)] 
-             -> Surface 
-             -> [Float] 
-             -> [Float] 
-             -> [Float] 
-             -> [Float] 
-             -> IO ()     
-lerpRows_ vc world lights slices tps surf p1x p1y p2x p2y = 
-         lerpRowsC (wallWidth vc) (modMask vc) (vcWindowWidth vc)
-                  16 16 world 
-                  slices 
-                  (map fst tps) -- (castPtr tps') 
-                  surf
-                  lights                  
-                  (fromIntegral (length lights))
-                  p1x p1y
-                  p2x p2y
-
--} 
-{-
-lerpRows_2 :: ViewConfig 
-             -> MapType 
-             -> Lights
-             -> [Int32] 
-             -> [(Pixels,Int32)] 
-             -> Surface 
-             -> [Float] 
-             -> [Float] 
-             -> [Float] 
-             -> [Float] 
-             -> IO ()     
-lerpRows_2 vc world lights slices tps surf p1x p1y p2x p2y = 
-         lerpRowsC_ (wallWidth vc) (modMask vc) (vcWindowWidth vc)
-                  16 16 world 
-                  slices 
-                  (map fst tps) 
-                  surf
-                  (lightsPtr lights)
-                  (lightsNum lights) -- (fromIntegral (length lights))
-                  p1x p1y
-                  p2x p2y
--} 
-lerpRows_2 :: ViewConfig 
-             -> MapType 
-             -> Lights
-             -> [Int32] 
-             -> [(Pixels,Int32)] 
-             -> Surface 
-             -> [RealLine] 
-             -> IO ()     
-lerpRows_2 vc world lights slices tps surf rl = 
-         lerpRowsC_  vc -- (wallWidth vc) (modMask vc) (vcWindowWidth vc)
-                  16 16 world 
-                  slices 
-                  (map fst tps) 
-                  surf
-                  (lightsPtr lights)
-                  (lightsNum lights) -- (fromIntegral (length lights))
-                  rl
-
-
-
-newFloorCastPoint :: ViewConfig -> MapType -> View -> Int32 -> Int32 -> (Float,Float)    
-newFloorCastPoint vc world (pos,angle) x y = 
-  ps
-  where 
-    radians = angle - columnAngle
-    columnAngle = atan (fromIntegral (x {-column-} - viewportCenterX vc) / fromIntegral (vcViewDistance vc))
-    
-    ps = ((point2DGetX pos) - distance * sin radians
-         ,(point2DGetY pos) + distance * cos radians)
-    
-    distance = rowDistance y 
-    
-    ratioHeightRow row = 128 {-fromIntegral viewerHeight-} / fromIntegral row 
-                         
-    
-    rowDistance row = ratioHeightRow row * fromIntegral (vcViewDistance vc) / cos columnAngle
-
-
-{-     
-lerpRow :: ViewConfig -> MapType -> [Light] -> Array Int Int32 -> Array Int (Ptr CInt,Int32) -> Surface -> (Int32, ((Float,Float),(Float,Float))) -> IO () 
-lerpRow vc world lights slices tps surf (y,(p1,p2)) = 
-  do 
-    sp <- castPtr `fmap` surfaceGetPixels surf
-    sequence_ [do 
-                  let (inx,iny) = ((floori_ (fromIntegral xi * rX+(fst p1))),
-                                   (floori_ (fromIntegral xi * rY+(snd p1)))) 
-                      (inR,inG,inB) = clamp 1.0 $ foldl1 vec3add (map (lightContribution (inx,iny))  lights)
-                      (wx,wy) = ((inx `div` wallWidth vc) .&. 15, 
-                                 (iny `div` wallWidth vc) .&. 15) 
-                      (tx,ty) = (inx .&. modMask vc,
-                                 iny .&. modMask vc)
-                      
-                  -- texture mapping    
-                  tix  <- fmap fromIntegral (world !! (wx,wy))
-                  let (tp,w) = (tps  ! tix ) 
-                      t      = tx + ty * (fromIntegral w) -- fromIntegral (surfaceGetWidth tex)   
-                 
-                  
-                
-                  if (slices ! (fromIntegral xi) <= y)     
-                     -- speeds up quite a bit when not much floor is visible
-                    then 
-                     do 
-                      texPointC tx ty w tp xi y width sp inR inG inB
-                      texPointC tx ty w tp xi (vcWindowHeight vc - y) width sp inR inG inB
-                    else 
-                      return () 
-                  {- 
-                  (p :: Word32) <- peekElemOff tp (fromIntegral t)  
-                  let p0  = p .&. 255 
-                      p1  = p `shiftR` 8 .&. 255 
-                      p2  = p `shiftR` 16 .&. 255 
-                      -- p3  = p `shiftR` 24 .&. 255 
-                      p0' =  floor_ $ inB * (fromIntegral p0) 
-                      p1' =  floor_ $ inG * (fromIntegral p1) 
-                      p2' =  floor_ $ inR * (fromIntegral p2) 
-                                
-                      p'  = p0' -- + (p1' `shiftL` 8) + (p2' `shiftL` 16)  -- + (p3' `shiftL` 24)
-                  pokeElemOff sp (fromIntegral (xi+y*width)) p'     -- floor... 
-                  -}
-              | xi <- [0..width-1]]
-    
-  where 
-    
-    rX = (fst p2 - fst p1) / fromIntegral width
-    rY = (snd p2 - snd p1) / fromIntegral width
-    width = vcWindowWidth vc
-    x1 = 0; 
-    
-
-   -} 
-
-
-
--- The slices are just there to be able to make some optimisations.              
-{-     
-floorCast :: ViewConfig -> MapType -> [Light] -> Point2D -> Angle -> [Slice] -> [Surface] -> Surface -> IO ()              
-floorCast vc world lights pos angle slices textures surf =              
-  zipWithM_ (floorCastColumn vc world lights pos angle textures surf) slices [0..vcWindowWidth vc -1]
-             
-floorCastColumn :: ViewConfig -> MapType -> [Light] -> Point2D -> Float -> [Surface] -> Surface -> Slice -> Int32 -> IO ()
-floorCastColumn vc world lights (px,py) angle tex surf slice col = 
-  do 
-    pixels <- castPtr `fmap` surfaceGetPixels surf 
-    texels <- mapM ((return . castPtr) <=< surfaceGetPixels) tex
-
-    sequence_ [renderPoint texels pixels r col xyd  
-               | (r,xyd)<- zip rows ps]  
-  where 
-    -- compute light 
-    radians = angle - columnAngle
-    columnAngle = atan (fromIntegral (col - viewportCenterX vc) / fromIntegral (vcViewDistance vc))
-    
-    -- only check floor where there are no walls (optimisation) 
-    rows = [sliceBot slice..vcWindowHeight vc-1]
-    -- rows = [viewportCenterY+1..windowHeight-1]              
-
-
-    ps = [(fromIntegral px - distance * sin radians 
-          ,fromIntegral py + distance * cos radians,distance )
-         | r <- rows
-         , let distance = rowDistance r] 
-    
-    ratioHeightRow row = 128 {-fromIntegral viewerHeight-} / fromIntegral (row - viewportCenterY vc) 
-                         
-    
-    rowDistance row = ratioHeightRow row * fromIntegral (vcViewDistance vc) / cos columnAngle
-         
-    renderPoint :: [Ptr Word32] -> Ptr Word32 -> Int32 -> Int32 -> (Float,Float,Float) -> IO ()      
-    renderPoint tex surf row col (x,y,dist) = 
-      do 
-        let (tx,ty) = (floori_ x `div` wallWidth vc, floori_ y `div` wallWidth vc) 
-        
-        index <- fmap fromIntegral (world !! (tx,ty))
-        p  <- peekElemOff (tex P.!! index)  (fromIntegral t) 
-        
-        let (inR,inG,inB) = clamp 1.0 $ foldl vec3add (0,0,0) (map (lightContribution (floori_ x,
-                                                                                       floori_ y))  lights)
-   
-        -- let i = (min 1.0 (32768/(dist*dist))) 
-        let p0  = p .&. 255 
-            p1  = p `shiftR` 8 .&. 255 
-            p2  = p `shiftR` 16 .&. 255 
-            -- p3  = p `shiftR` 24 .&. 255 
-            p0' =  floor_ $ inB * (fromIntegral p0) 
-            p1' =  floor_ $ inG * (fromIntegral p1) 
-            p2' =  floor_ $ inR * (fromIntegral p2) 
-                                
-            p'  = p0' + (p1' `shiftL` 8) + (p2' `shiftL` 16)  -- + (p3' `shiftL` 24)
-        
-        pokeElemOff surf (fromIntegral r)  p'     -- floor... 
-        pokeElemOff surf (fromIntegral r2) p'     -- ceiling...   
-        
-        where 
-          t  = ((floori_ y .&. modMask vc) * textureWidth + (floori_ x .&. modMask vc))
-          r  = (row * (vcWindowWidth vc) + col)
-          r2 = ((vcWindowHeight vc -row-1) * (vcWindowWidth vc) + col )
-          textureWidth = 256 -- fix this !!! surfaceGetWidth tex
-  
--} 
 ----------------------------------------------------------------------------
 -- Main !
 main = do 
@@ -455,6 +156,8 @@ main = do
                             ,conv pf =<< loadBMP "../../Data/Floor3.bmp"
                             ,conv pf =<< loadBMP "../../Data/Brunn.bmp"]
   
+  testLevelArr <- mkMap (16,16) testLevel testFloor testFloor wallTextures floorTextures floorTextures
+  
   --floorTextures <- sequence [conv pf =<< loadBMP "Data/Floor.bmp"
   --                          ,conv pf =<< loadBMP "Data/floor1.bmp"
   --                          ,conv pf =<< loadBMP "Data/floor2.bmp"
@@ -481,6 +184,7 @@ main = do
     (False,False,False,False) -- Keyboard state
     (0.0,mkPoint (256+128 ,256+128))
     0
+    testLevelArr
     
   quit
     where 
@@ -497,8 +201,9 @@ eventLoop :: ViewConfig
              -> (Bool,Bool,Bool,Bool) 
              -> (Float,Point2D) 
              -> Int32
+             -> MapType
              -> IO ()
-eventLoop vc screen floorTextures wallTextures monster targ (up,down,left,right) (r,pos) ly = do 
+eventLoop vc screen floorTextures wallTextures monster targ (up,down,left,right) (r,pos) ly testLevelArr = do 
   
   let (x,y) = (point2DGetX pos, point2DGetY pos)
       lights = ([mkLight (x,y) (1.0,1.0,1.0)] ++ 
@@ -515,8 +220,8 @@ eventLoop vc screen floorTextures wallTextures monster targ (up,down,left,right)
                          screen
                          
        let dists  = map sliceDistance sl
-           bots   = map sliceBot      sl 
-       newFloorCast3 vc testLevelFloorArr lights' bots (pos,r) floorTextures screen                                     
+         --  bots   = map sliceBot      sl 
+       --newFloorCast3 vc testLevelFloorArr lights' bots (pos,r) floorTextures screen                                     
 
        
        let monsterTfrmd = viewTransformSprite vc (pos,r) monster
@@ -581,7 +286,7 @@ eventLoop vc screen floorTextures wallTextures monster targ (up,down,left,right)
   (r',x',y') <- (moveLeft left' >=> moveRight right' >=> moveUp up' >=> moveDown down') (r,x,y) 
   
   let pos' = mkPoint (x',y')
-  unless b $ eventLoop vc screen floorTextures wallTextures monster' targ' (up',down',left',right') (r',pos') ((ly + 128) `mod` 4096)    
+  unless b $ eventLoop vc screen floorTextures wallTextures monster' targ' (up',down',left',right') (r',pos') ((ly + 128) `mod` 4096) testLevelArr   
   
   
   -- very crude colision against walls added
